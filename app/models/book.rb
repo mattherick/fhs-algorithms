e8983b1851900b44e023d3a7c5953ffef32a1c7b
Bettina Steger <bettina_steger@gmx.at>
2012-10-29 19:55:38 +0100

    task 1: arithmetic avg, median and quantil

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,6 +4,52 @@ class Book < ActiveRecord::Base
   
   has_many :ratings, :foreign_key => "ISBN"
   has_many :users, :through => :ratings, :foreign_key => ["User-ID", "ISBN"]
+
+  # Task 1
+  # Calculate 𝑥 ,𝑥 and 𝑥 0.25 of the ratings of books "0316095648", "0971880107", and "0446610038". 
+  # What is the message of those values?
+  def self.task1
+      
+  end
+
+  # Book.find("0316095648").arithmetic_avg
+  # Book.find("0971880107").arithmetic_avg
+  # Book.find("0446610038").arithmetic_avg
+  def arithmetic_avg
+    ratings.map(&:'Book-Rating').sum.to_f / ratings.count
+  end
+
+  # Book.find("0316095648").median
+  # Book.find("0971880107").median
+  # Book.find("0446610038").median
+  def median
+    ratings_array = ratings.map(&:'Book-Rating').sort!
+    
+    if ratings_array.count % 2 == 0 # even
+      i = ratings_array.count/2
+      median = (ratings_array[i-1] + ratings_array[i])/2
+    else # odd
+      i = (ratings_array.count-1)/2
+      median = ratings_array[i]
+    end
+    median
+  end
+
+  # Book.find("0316095648").quantil
+  # Book.find("0971880107").quantil
+  # Book.find("0446610038").quantil
+  def quantil(p=0.25)
+    ratings_array = ratings.map(&:'Book-Rating').sort!
+    np = ratings_array.count * p
+    
+    if np.integer?  # 𝑛𝑝 ∈ ℕ
+      quantil = (ratings_array[np-1] + ratings_array[np])/2
+    else # 𝑛𝑝 ∉ ℕ
+      quantil = ratings_array[np.to_i]
+    end
+    quantil
+  end
+
 end
 
 # Schema
